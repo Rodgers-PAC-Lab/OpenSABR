92c498b3a5b9758a38f4a3c506151515ca6ee9f8
PAC Lab <rodgers.paclab@gmail.com>
2026-08-13 18:03:02 -0400

    Launcher script popped out of gui subdirectory

--- src/opensabr/start_gui.sh
+++ src/opensabr/start_gui.sh
@@ -2,9 +2,10 @@
 # This must be run with "bash -i" for conda to work
 
 # -u makes python run in unbuffered mode so we can see error messages
-conda activate py3 
 
-python3 -u -m ABR2025.gui.start_gui |& tee -a /home/mouse/mnt/cuttlefish/surgery/abr_logfiles/logfile.$(date '+%F-%T-%N')
+conda activate opensabr 
+
+python3 -u -m opensabr.gui.start_gui |& tee -a /home/mouse/mnt/cuttlefish/surgery/abr_logfiles/logfile.$(date '+%F-%T-%N')
 
 echo ABR GUI closed. Press any key to close this window. 
 echo PAC Lab personnel - do not ignore this message\! Close this window\!
